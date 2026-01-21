77ecb5e5b9b1dfb593b748654507d2804e465c39
Kyle Martinez <kyletmartinez@gmail.com>
2026-01-20 19:06:06 -0600

    simplify header

--- Embed External Assets Into Lottie.js
+++ Embed External Assets Into Lottie.js
@@ -3,40 +3,7 @@
  * @version 1.0
  * @author Kyle Martinez <www.kyle-martinez.com>
  *
- * @description Embed all external assets into Lottie.
- *
- * @installation
- * 1. Save this file as "Embed External Assets Into Lottie.js" into your Cavalry scripts folder
- * 2. Find the scripts folder via: Help > Show Scripts Folder (or Scripts > Show Scripts Folder)
- * 3. Restart Cavalry or refresh the Scripts menu
- * 4. Access via: Window > Scripts > Embed External Assets Into Lottie
- *
- * @usage
- * 1. Open the original .cv file used to generate the Lottie OR generate a new Lottie with the
- *    current file
- * 2. ENABLE or DISABLE the "Center anchor points" feature
- * 3. Click the "Select JSON" button and select the existing Lottie JSON file
- * 4. If successful the old JSON will be saved with a ".bak" extension and the new JSON will have
- *    all external assets embedded into the JSON file
- *
- * @discoveries
- * 1. LOTTIE EMBEDDED ASSETS FEATURE MISSING
- *     - Cavalry 2.5.4 does not support embedded images when exporting to Lottie
- *     - It generates a sidecar "images/" folder which is not ideal for single file deliverables or
- *       easy testing during render
- *
- * 2. LOTTIE EXPORT BUG
- *     - Cavalry 2.5.4 does not correctly include all image assets in the sidecar "images/" folder
- *     - It appears exports with a single image work but those with 2+ do not
- *     - On export image files have appended digits (ex: FileName0.png, FileName1.png)
- *     - This doesn't appear to be an issue as the Lottie "assets" array contains these updated
- *       file names
- *
- * 3. ANCHOR POINT OFFSET
- *     - Simply replacing the external assets with base64 embeds (similar to Bodymovin) works well
- *       however images appear to be using a bottom-right anchor point
- *     - Adjusting anchor points using negative half width and negative half height values appear to
- *       fix this issue
+ * @description Embed external assets into Lottie from the original Cavalry file
  *
  * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
  * no event shall the author be held liable for any damages arising in any way from the use of this
