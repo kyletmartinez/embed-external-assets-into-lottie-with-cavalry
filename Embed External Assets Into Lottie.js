c7acd4d6704aa03a5ac88116da91e6a83d9be36e
Kyle Martinez <kyletmartinez@gmail.com>
2026-01-20 19:07:53 -0600

    version up

--- Embed External Assets Into Lottie.js
+++ Embed External Assets Into Lottie.js
@@ -1,6 +1,6 @@
 /**
  * @name Embed External Assets Into Lottie
- * @version 1.0
+ * @version 1.1
  * @author Kyle Martinez <www.kyle-martinez.com>
  *
  * @description Embed external assets into Lottie from the original Cavalry file
